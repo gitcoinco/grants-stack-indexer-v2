e839723015f662867708394b3c09e21e385040c1
nigiri <168690269+0xnigir1@users.noreply.github.com>
2025-02-11 14:24:37 -0300

    fix: pr comments

--- packages/shared/src/notifier/notifierConfig.interface.ts
+++ packages/shared/src/notifier/notifierConfig.interface.ts
@@ -2,11 +2,21 @@ import { SlackOptions } from "./slack.notifier.js";
 
 export type NotifierProvider = "slack" | "null";
 
+/**
+ * Configuration type for the Slack notifier.
+ * @property notifierProvider - Must be set to "slack" to use Slack notifications.
+ * @property opts - Slack-specific configuration options.
+ */
 export type SlackNotifierConfig = {
     notifierProvider: "slack";
     opts: SlackOptions;
 };
 
+/**
+ * Generic configuration type for notifiers.
+ * @template Source - The type of notifier provider to configure.
+ * @returns Configuration type specific to the provider: SlackNotifierConfig for "slack", or a null configuration otherwise.
+ */
 export type NotifierConfig<Source extends NotifierProvider> = Source extends "slack"
     ? SlackNotifierConfig
     : {
