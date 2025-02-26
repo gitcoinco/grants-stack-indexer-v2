1a51ce6baaf615a25cf98d6f49fb6ebf2bd0d1a1
nigiri <168690269+0xnigir1@users.noreply.github.com>
2025-02-26 10:32:55 -0300

    feat: index GAN in Envio for Sepolia

---
+++ apps/indexer/src/handlers/GitcoinAttestationNetwork.ts
@@ -0,0 +1,6 @@
+// /*
+//  * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
+//  */
+import { GitcoinAttestationNetwork } from "../../generated";
+
+GitcoinAttestationNetwork.OnAttested.handler(async ({}) => {});
