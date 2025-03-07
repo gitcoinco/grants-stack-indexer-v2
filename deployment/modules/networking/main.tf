a647bec5252caa137f50cb7d00f656238ac389e4
0xkenj1 <kenji@defi.sucks>
2025-03-07 15:49:44 -0300

    fix: add cidr_blocks

--- deployment/modules/networking/main.tf
+++ deployment/modules/networking/main.tf
@@ -145,9 +145,10 @@ resource "aws_security_group" "api_without_lb" {
   }
 
   ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
