b505de2db37cbbf67349dc9f95cf3d7ee6e78495
Huss Martinez <husse.dev@gmail.com>
2025-05-26 19:59:25 +0700

    chore: allow access to RDS from any source

--- deployment/modules/networking/main.tf
+++ deployment/modules/networking/main.tf
@@ -71,6 +71,14 @@ resource "aws_security_group" "rds" {
     cidr_blocks = module.vpc.public_subnets_cidr_blocks # Allow access from public subnets
   }
 
+  # Allow access from anywhere (0.0.0.0/0)
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
